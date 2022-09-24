d9bb0b8955795402e185a9a89c0b8f1344e9aa9b
Олег <GrishutinOV@yandex.ru>
2022-09-24 20:34:18 +0300

    The code works need to add contraint about 5 last redords

--- src/main/scala/SimleKafkaConsumer.scala
+++ src/main/scala/SimleKafkaConsumer.scala
@@ -1,8 +1,11 @@
 import java.util.{Collections, Properties}
 import java.util.regex.Pattern
 import org.apache.kafka.clients.consumer.KafkaConsumer
+
 import scala.collection.JavaConverters._
 import org.apache.kafka.common.serialization.Deserializer
+
+import java.time.Duration
 
 
 
@@ -15,14 +18,16 @@ object SimleKafkaConsumer extends App {
     "org.apache.kafka.common.serialization.StringDeserializer")
   props.put("value.deserializer",
     "org.apache.kafka.common.serialization.StringDeserializer")
-  props.put("enable.auto.commit", "true")
+  //props.put("enable.auto.commit", "true")
   props.put("auto.commit.interval.ms", "1000")
   val consumer = new KafkaConsumer(props)
   val topics = List("amazon_books")
   try {
     consumer.subscribe(topics.asJava)
     while (true) {
-      val records = consumer.poll(10)
+      val records = consumer.poll(Duration.ofSeconds(1))
+      val topicPartitions = consumer.assignment()
+      consumer.seekToBeginning(topicPartitions)
       for (record <- records.asScala) {
         println("Topic: " + record.topic() +
           ",Key: " + record.key() +
